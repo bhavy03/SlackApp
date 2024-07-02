de7da047a906452f92ba97fee73650bce4296e16
Bhavya <bhavya7664@gmail.com>
2024-07-02 11:39:35 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Project The Daily Bugle
+
+Develop an elegant React.js News portal.
+
+# Contributing
+
+When contributing to this repository, please first discuss the change you wish to make via issue.
+
+## System Requirements
+
+To get started with development, you need to install few tools
+
+1. git
+
+   `git` version 2.13.1 or higher. Download [git](https://git-scm.com/downloads) if you don't have it already.
+
+   To check your version of git, run:
+
+   ```shell
+    git --version
+   ```
+
+2. node
+
+   `node` version 16.15.1 or higher. Download [node](https://nodejs.org/en/download/) if you don't have it already.
+
+   To check your version of node, run:
+
+   ```shell
+    node --version
+   ```
+
+3. npm
+
+   `npm` version 5.6.1 or higher. You will have it after you install node.
+
+   To check your version of npm, run:
+
+   ```shell
+    npm --version
+   ```
+
+## Setup
+
+To set up a development environment, please follow these steps:
+
+1. Clone the repo
+
+   ```shell
+    git clone https://github.com/bhavy03/NewsPortal.git
+   ```
+
+2. Install the dependencies
+
+   ```shell
+    npm install
+   ```
+
+   If you get an error, please check the console for more information.
+
+   If you don't get an error, you are ready to start development.
+
+
+3. Change config.env.example
+
+   Change the filename from config.env.example to
+config.env and assign all variables according to you
+
+
+4. Run the app
+
+   ```shell
+   npm run dev
+   ```
+
+   Project will be running in the browser.
+
+   Open [http://localhost:5173](http://localhost:5173) to view it in your browser.
---
+++ index.js
@@ -0,0 +1,124 @@
+const { App } = require("@slack/bolt");
+const signingSecret = process.env.SLACK_SIGNING_SECRET;
+const botToken = process.env.SLACK_BOT_TOKEN;
+const appToken = process.env.APP_TOKEN;
+
+const app = new App({
+  signingSecret: `${signingSecret}`,
+  token: `${botToken}`,
+  socketMode: true, // enable to use socket mode
+  appToken: `${appToken}`,
+});
+
+(async () => {
+  await app.start(process.env.PORT || 12000);
+  app.message("hey", async ({ command, say }) => {
+    try {
+      say("Hello Human!");
+    } catch (error) {
+      console.log("err");
+      console.error(error);
+    }
+  });
+  app.shortcut("new_id", async ({ shortcut, ack, client }) => {
+    await ack();
+    try {
+      await client.views.open({
+        trigger_id: shortcut.trigger_id,
+        view: {
+          type: "modal",
+          callback_id: "modal_view",
+          title: {
+            type: "plain_text",
+            text: "InternBit Assignment",
+            emoji: true,
+          },
+          submit: {
+            type: "plain_text",
+            text: "Submit",
+            emoji: true,
+          },
+          close: {
+            type: "plain_text",
+            text: "Cancel",
+            emoji: true,
+          },
+          blocks: [
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "Select a user",
+              },
+              accessory: {
+                type: "users_select",
+                placeholder: {
+                  type: "plain_text",
+                  text: "Select a user",
+                  emoji: true,
+                },
+                action_id: "users_select-action",
+              },
+            },
+            {
+              type: "input",
+              element: {
+                type: "rich_text_input",
+                action_id: "rich_text_input-action",
+              },
+              label: {
+                type: "plain_text",
+                text: "Message",
+                emoji: true,
+              },
+            },
+          ],
+        },
+      });
+    } catch (error) {
+      console.log("err");
+      console.error(error);
+    }
+  });
+  app.view("modal_view", async ({ ack, body, view, client }) => {
+    await ack();
+
+    // Extract the user and message from the view state
+    const userBlock = view.state.values["H5UyI"];
+    const messageBlock = view.state.values["ey271"];
+
+    const user = userBlock["users_select-action"].selected_user;
+    const richTextValue =
+      messageBlock["rich_text_input-action"].rich_text_value;
+
+    function extractPlainText(richTextValue) {
+      let plainText = "";
+      richTextValue.elements.forEach((element) => {
+        if (element.type === "rich_text_section") {
+          element.elements.forEach((subElement) => {
+            if (subElement.type === "text") {
+              plainText += subElement.text;
+            }
+          });
+        }
+      });
+      return plainText;
+    }
+
+    const message = extractPlainText(richTextValue);
+
+    console.log("Selected User:", user);
+    console.log("Message:", message);
+    // Send a message to the selected user
+    try {
+      await client.chat.postMessage({
+        channel: user,
+        text: message,
+      });
+      console.log(`Message sent to user ${user}`);
+    } catch (error) {
+      console.error("Error sending message to user:", error);
+    }
+  });
+  console.log(`⚡️ Bolt app is running!`);
+})();
